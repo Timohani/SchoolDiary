445bf4cdf1873e85698d9e14d6fedbe1283bb1ce
timohani <sd3059272@gmail.com>
2025-10-08 21:35:20 +0500

    Init

---
+++ src/main/java/com/timohani/schooldiary/SchooldiaryApplication.java
@@ -0,0 +1,13 @@
+package com.timohani.schooldiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchooldiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchooldiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/timohani/schooldiary/SchooldiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timohani.schooldiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchooldiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
